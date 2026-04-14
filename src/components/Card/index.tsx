4c179de1c683947e41a1d3690f99dc7ad53012ab
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-04-13 22:35:15 -0300

    modified components of card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,21 @@
 import type { CardProps } from "../../interfaces/CardProps";
+import ButtonRemove from "../Button";
 import './style.css'
 
 export default function Card({ name, time } : CardProps) {
+
+    async function handleButtonRemove() {
+        console.log('Removendo card:', name);
+    }
+
     return (
         <div className="content-card">
             <div className="card">
                 <strong>{name}</strong>
                 <small>{time}</small>
+            </div>
+            <div className="btn-remove">
+                <ButtonRemove name="Remover" onClick={handleButtonRemove}/>
             </div>
         </div>
     )
